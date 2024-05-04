3b0b4ee7051e607aeb5e56b53ec9dd8c42ae4af2
trevortrues <123995584+trevortrues@users.noreply.github.com>
2024-05-04 17:41:33 -0500

    block 'game'
    Added blocks with collision functionality. Doesn't work perfectly

--- src/main.rs
+++ src/main.rs
@@ -1,68 +1,99 @@
 use bevy::prelude::*;
 use bevy::render::camera::ScalingMode;
-#[allow(unused)]
-#[allow(dead_code)]
 
 const SKY_COLOR: Color = Color::rgb(135.0 / 255.0, 206.0 / 255.0, 250.0 / 255.0);
+const MOVE_SPEED: f32 = 100.0;
+const FALL_SPEED: f32 = 98.0;
 
 #[derive(Debug, Component, Clone, Copy)]
 struct HitBox(Vec2);
 
 #[derive(Component)]
-pub struct Player{
-    pub speed: f32
+pub struct Player {
+    pub speed: f32,
 }
 
 #[derive(Resource)]
 pub struct Coins(pub f32);
 
-
-
-
-fn setup(mut commands: Commands, asset_server: ResMut<AssetServer>) {
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     let mut camera = Camera2dBundle::default();
-    
-    camera.projection.scaling_mode = ScalingMode::AutoMin{
+    camera.projection.scaling_mode = ScalingMode::AutoMin {
         min_width: 256.0,
-        min_height:144.0
+        min_height: 144.0,
     };
-   
     commands.spawn(camera);
 
-    let texture = asset_server.load("pixil-frame-0.png");
-
+    let player_texture = asset_server.load("pixil-frame-0.png");
     commands.spawn((
         SpriteBundle {
-            texture,
-            
+            texture: player_texture,
+            transform: Transform {
+                translation: Vec3::new(0.0, 0.0, 1.0),
+                scale: Vec3::splat(1.0),
+                ..default()
+            },
+            sprite: Sprite {
+                custom_size: Some(Vec2::splat(32.0)),
+                ..default()
+            },
             ..default()
         },
-        Player{speed: 100.0},
+        Player { speed: 100.0 },
         Grounded(true),
+        HitBox(Vec2::new(32.0, 32.0)),
     ));
-}
 
-const FALL_SPEED: f32 = 98.0;
+    let block_texture = asset_server.load("brick.png");
+    let block_positions = vec![
+        Vec3::new(-64.0, -64.0, 0.0),   
+        Vec3::new(64.0, -64.0, 0.0),   
+        Vec3::new(0.0, -128.0, 0.0),    
+        Vec3::new(0.0, 128.0, 0.0),   
+    ];
+
+    for position in block_positions {
+        commands.spawn((
+            SpriteBundle {
+                texture: block_texture.clone(),
+                transform: Transform {
+                    translation: position,
+                    scale: Vec3::splat(1.0),
+                    ..default()
+                },
+                sprite: Sprite {
+                    custom_size: Some(Vec2::splat(32.0)),
+                    ..default()
+                },
+                ..default()
+            },
+            HitBox(Vec2::new(32.0, 32.0)),
+        ));
+    }
+}
 
 #[derive(Component)]
 struct Jump(f32);
 
-
 fn player_jump(
-    mut commands: Commands, time: Res<Time>,
+    mut commands: Commands,
+    time: Res<Time>,
     mut player: Query<(Entity, &mut Transform, &mut Jump), With<Player>>,
 ) {
-    let Ok((player, mut transform, mut jump)) = player.get_single_mut() else {return;};
-    let jump_power = (time.delta_seconds() * FALL_SPEED *2.).min(jump.0);
+    let Ok((player, mut transform, mut jump)) = player.get_single_mut() else { return; };
+    let jump_power = (time.delta_seconds() * FALL_SPEED * 2.).min(jump.0);
     jump.0 -= jump_power;
     transform.translation.y += jump_power;
-    if jump.0 == 0. {commands.entity(player).remove::<Jump>();}
+    if jump.0 == 0. {
+        commands.entity(player).remove::<Jump>();
+    }
 }
+
 fn player_fall(
     mut player: Query<&mut Transform, (With<Player>, Without<Jump>)>,
     time: Res<Time>,
 ) {
-    let Ok(mut player) = player.get_single_mut() else {return;};
+    let Ok(mut player) = player.get_single_mut() else { return; };
     if player.translation.y > 0.0 {
         player.translation.y -= time.delta_seconds() * FALL_SPEED;
         if player.translation.y < 0.0 {
@@ -71,38 +102,68 @@ fn player_fall(
     }
 }
 
-
 fn character_movement(
     mut commands: Commands,
-    mut characters: Query<(&mut Transform, &Player, Option<&Jump>, Entity)>,
+    mut player: Query<(&mut Transform, &Player, Option<&Jump>, Entity, &HitBox)>,
+    blocks: Query<(&HitBox, &Transform), Without<Player>>,
     input: Res<Input<KeyCode>>,
     time: Res<Time>,
 ) {
-    for (mut transform, player, jump, entity) in characters.iter_mut() {
+    for (mut transform, player, jump, entity, player_hitbox) in player.iter_mut() {
         let movement_amt = player.speed * time.delta_seconds();
-        if input.just_pressed(KeyCode::W) && jump.is_none() {
-            commands.entity(entity).insert(Jump(25.0));
-        }
+        let mut delta_x = 0.0;
+        let mut delta_y = 0.0;
+
         if input.pressed(KeyCode::A) {
-            transform.translation.x -= movement_amt;
-        }
-        if input.pressed(KeyCode::S) && transform.translation.y < 600.0 {
-            transform.translation.y -= movement_amt;
+            delta_x = -movement_amt;
         }
         if input.pressed(KeyCode::D) {
-            transform.translation.x += movement_amt;
+            delta_x = movement_amt;
+        }
+        if input.pressed(KeyCode::S) && transform.translation.y < 600.0 {
+            delta_y = -movement_amt;
+        }
+
+        let horizontal_pos = transform.translation + Vec3::X * delta_x;
+        if !is_colliding(&horizontal_pos, player_hitbox, &blocks) {
+            transform.translation.x = horizontal_pos.x;
+        }
+
+        let vertical_pos = transform.translation + Vec3::Y * delta_y;
+        if !is_colliding(&vertical_pos, player_hitbox, &blocks) {
+            transform.translation.y = vertical_pos.y;
+        }
+
+        // Jumping logic
+        if input.just_pressed(KeyCode::W) && jump.is_none() {
+            commands.entity(entity).insert(Jump(25.0));
         }
     }
 }
 
-fn check_hit(hitbox: HitBox, offset: Vec3, other_hitbox: HitBox, other_offset: Vec3) -> bool {
-    let h_size = hitbox.0.y /2.;
-    let oh_size = other_hitbox.0.y /2.;
-    let w_size = hitbox.0.x /2.;
-    let ow_size = other_hitbox.0.x /2.;
+fn is_colliding(
+    new_position: &Vec3,
+    player_hitbox: &HitBox,
+    blocks: &Query<(&HitBox, &Transform), Without<Player>>,
+) -> bool {
+    for (block_hitbox, block_transform) in blocks.iter() {
+        if check_hit(*player_hitbox, *new_position, *block_hitbox, block_transform.translation) {
+            return true;
+        }
+    }
+    false
+}
 
-    offset.x + w_size > other_offset.x - ow_size && offset.x - w_size < other_offset.x + ow_size &&
-    offset.y + h_size > other_offset.y - oh_size && offset.y - h_size < other_offset.y + oh_size
+fn check_hit(hitbox: HitBox, offset: Vec3, other_hitbox: HitBox, other_offset: Vec3) -> bool {
+    let h_size = hitbox.0.y / 2.;
+    let oh_size = other_hitbox.0.y / 2.;
+    let w_size = hitbox.0.x / 2.;
+    let ow_size = other_hitbox.0.x / 2.;
+
+    offset.x + w_size > other_offset.x - ow_size &&
+        offset.x - w_size < other_offset.x + ow_size &&
+        offset.y + h_size > other_offset.y - oh_size &&
+        offset.y - h_size < other_offset.y + oh_size
 }
 
 #[derive(Component)]
@@ -112,18 +173,13 @@ fn ground_detection(
     mut player: Query<(&Transform, &mut Grounded), With<Player>>,
     mut last: Local<Transform>,
 ) {
-    let (pos,mut on_ground) = player.single_mut();
-    let current = if pos.translation.y == last.translation.y {
-        true
-    } else {
-        false
-    };
+    let (pos, mut on_ground) = player.single_mut();
+    let current = pos.translation.y == last.translation.y;
     if current != on_ground.0 {
         on_ground.0 = current;
     }
     *last = *pos;
 }
-
 
 fn main() {
     App::new()
